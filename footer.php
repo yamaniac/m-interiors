31392a30122cc50ad283eae90eefb8153f267c02
Manjeeth <manjeeth.shenoy@gmail.com>
2016-02-11 23:28:33 +0530

    made changes
    added gallery to home page

---
+++ footer.php
@@ -0,0 +1,64 @@
+<div class="footer">
+<div class="container">
+ 
+  <div class="row">
+    <div class="col-md-4 buffer-text">
+    
+Copyrights 2016 - Maestrorealestate.com. All Rights Reserved.
+    </div>
+    
+        <div class="col-md-4 flinks buffer-text">
+<ul>
+<li><a href="index.php">Home</a>  |</li>
+<li><a href="#"> About Us</a> |</li>
+<li><a href="#">Projects</a> |</li>
+<li><a href="#">testimonials</a> |</li>
+<li><a href="contact.php">Contact Us</a> </li>
+
+
+</ul>    
+
+    </div>
+    
+<div class="col-md-2 text-center">
+ <img src="images/credai.jpg" width="96" height="22" />
+    </div>
+
+<div class="col-md-2 text-right"> Powered By: <a href="#" title="Pacesoft Technologies"> <img src="images/pacesoft.jpg" width="24" height="24" /></a> </div>    
+  
+  </div>
+</div>
+</div>
+
+<!-- /.container -->
+<!-- core files  needed to run this site smoothly -->
+<script src="js/jquery.js" type="text/javascript"></script> 
+<script src="js/bootstrap.min.js" type="text/javascript"></script>
+<script src="js/mixit.js" type="text/javascript"></script>
+<script src="js/waypoint.js" type="text/javascript"></script>
+<script src="js/scroll.js" type="text/javascript"></script>
+<script src="js/custom.js" type="text/javascript"></script>
+
+<script src="https://maps.googleapis.com/maps/api/js?callback=initMap" async defer>
+        </script>
+<script>
+  function initMap() {
+    var mapDiv = document.getElementById('map');
+    var map = new google.maps.Map(mapDiv, {
+      center: {lat: 44.540, lng: -78.546},
+      zoom: 8
+    });
+  }
+</script>   
+        
+<script>
+    $('.carousel').carousel({
+        interval: 5000 //changes the speed
+    })
+    
+
+    
+    
+    </script>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,398 @@
+<?php include 'header.php'; ?>
+<?php include 'menu.php'; ?>
+
+<!-- Header Carousel -->
+<header class="carousel slide" id="myCarousel">
+<!-- Indicators -->
+<ol class="carousel-indicators">
+	<li class="active" data-slide-to="0" data-target="#myCarousel">
+	</li>
+	<li data-slide-to="1" data-target="#myCarousel">
+	</li>
+	<li data-slide-to="2" data-target="#myCarousel">
+	</li>
+</ol>
+
+
+
+<div class="carousel-inner">
+            <div class="item active">
+                <div class="fill" style="background-image:url('http://placehold.it/1900x1080&text=Slide One');">
+
+                </div>
+
+
+                <div class="carousel-caption">
+                    <h2>Caption 1</h2>
+                </div>
+            </div>
+
+
+            <div class="item">
+                <div class="fill" style=
+                "background-image:url('http://placehold.it/1900x1080&text=Slide Two');">
+                </div>
+
+
+                <div class="carousel-caption">
+                    <h2>Caption 2</h2>
+                </div>
+            </div>
+
+
+            <div class="item">
+                <div class="fill" style=
+                "background-image:url('http://placehold.it/1900x1080&text=Slide Three');">
+                </div>
+
+
+                <div class="carousel-caption">
+                    <h2>Caption 3</h2>
+                </div>
+            </div>
+        </div>
+        <!-- Controls -->
+         <a class="left carousel-control" data-slide="prev" href=
+        "#myCarousel"><span class="icon-prev"></span></a> <a class=
+        "right carousel-control" data-slide="next" href=
+        "#myCarousel"><span class="icon-next"></span></a>
+    </header>
+    <!-- Page Content -->
+    <div class="top-buffer"></div>
+ <!-- Intro Content -->
+ <div class="container">
+        <div class="row">
+            <div class="col-md-6">
+                <img class="img-responsive" src="http://placehold.it/750x450" alt="">
+            </div>
+            <div class="col-md-6">
+                <h2 class=" descp sc_title"><strong>About</strong> Modern Interiors</h2>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sed voluptate nihil eum consectetur similique? Consectetur, quod, incidunt, harum nisi dolores delectus reprehenderit voluptatem perferendis dicta dolorem non blanditiis ex fugiat.</p>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Saepe, magni, aperiam vitae illum voluptatum aut sequi impedit non velit ab ea pariatur sint quidem corporis eveniet. Odit, temporibus reprehenderit dolorum!</p>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Et, consequuntur, modi mollitia corporis ipsa voluptate corrupti eum ratione ex ea praesentium quibusdam? Aut, in eum facere corrupti necessitatibus perspiciatis quis?</p>
+            </div>
+        </div>
+        </div>
+        <!-- /.row -->
+<div class="container">
+	<!-- Portfolio Section -->
+	<div class="container">
+		<div class="row">
+			<div class="col-md-12 descp">
+				<h2 class=" center-block descp sc_title">
+				<strong>Recent</strong> Projects</h2>
+				<p>
+					Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
+				</p>
+			</div>
+		</div>
+	</div>
+	<div class="row top-buffer">
+	 <div class="col-md-4 col-sm-6 os-animation" data-os-animation="bounceIn" data-os-animation-delay="1s"> 
+     <div class="wrapper">
+     <div class="project_details ">
+     <ul>
+     <li class="pull-left">Project Name</li>
+     <li class="pull-right"><i class="fa fa-map-marker"></i>
+ Location</li>
+     </ul>
+      </div>
+               <a href="portfolio-item.html"><img alt="" class=
+                "img-responsive img-portfolio img-hover " src=
+                "http://placehold.it/700x450"></a>
+                   </div>
+            </div>
+	 <div class="col-md-4  col-sm-6 os-animation" data-os-animation="bounceIn" data-os-animation-delay="1s">
+                <div class="wrapper">
+     <div class="project_details">
+     <ul>
+     <li class="pull-left">Project Name</li>
+     <li class="pull-right"><i class="fa fa-map-marker"></i>
+ Location</li>
+     </ul>
+      </div>
+               <a href="portfolio-item.html"><img alt="" class=
+                "img-responsive img-portfolio img-hover" src=
+                "http://placehold.it/700x450"></a>
+                   </div>
+            </div>
+	 <div class="col-md-4 col-sm-6 os-animation" data-os-animation="bounceIn" data-os-animation-delay="1s">
+               <div class="wrapper">
+     <div class="project_details">
+     <ul>
+     <li class="pull-left">Project Name</li>
+     <li class="pull-right"><i class="fa fa-map-marker"></i>
+ Location</li>
+     </ul>
+      </div>
+               <a href="portfolio-item.html"><img alt="" class=
+                "img-responsive img-portfolio img-hover" src=
+                "http://placehold.it/700x450"></a>
+                   </div>
+            </div>
+	 <div class="col-md-4 col-sm-6 os-animation" data-os-animation="bounceIn" data-os-animation-delay="1s">
+                 <div class="wrapper">
+     <div class="project_details">
+     <ul>
+     <li class="pull-left">Project Name</li>
+     <li class="pull-right"><i class="fa fa-map-marker"></i>
+ Location</li>
+     </ul>
+      </div>
+               <a href="portfolio-item.html"><img alt="" class=
+                "img-responsive img-portfolio img-hover" src=
+                "http://placehold.it/700x450"></a>
+                   </div>
+            </div>
+	 <div class="col-md-4 col-sm-6 os-animation" data-os-animation="bounceIn" data-os-animation-delay="1s">
+               <div class="wrapper">
+     <div class="project_details">
+     <ul>
+     <li class="pull-left">Project Name</li>
+     <li class="pull-right"><i class="fa fa-map-marker"></i>
+ Location</li>
+     </ul>
+      </div>
+               <a href="portfolio-item.html"><img alt="" class=
+                "img-responsive img-portfolio img-hover" src=
+                "http://placehold.it/700x450"></a>
+                   </div>
+            </div>
+	 <div class="col-md-4 col-sm-6 os-animation" data-os-animation="bounceIn" data-os-animation-delay="1s">
+                <div class="wrapper">
+     <div class="project_details">
+     <ul>
+     <li class="pull-left">Project Name</li>
+     <li class="pull-right"><i class="fa fa-map-marker"></i>
+ Location</li>
+     </ul>
+      </div>
+               <a href="portfolio-item.html"><img alt="" class=
+                "img-responsive img-portfolio img-hover" src=
+                "http://placehold.it/700x450"></a>
+                   </div>
+            </div>
+		<div class="container">
+			<div class="row">
+				<div class="col-md-12 center-block-item">
+					<a class="btn btn-danger btn-lg os-animation" data-os-animation="bounceInDown" data-os-animation-delay="1s" href="#">View All Projects</a>
+				</div>
+			</div>
+		</div>
+	</div>
+    
+<div class="controls">
+  <label>Filter:</label>
+  
+  <button class="filter" data-filter="all">All</button>
+  <button class="filter" data-filter=".category-1">Category 1</button>
+  <button class="filter" data-filter=".category-2">Category 2</button>
+  
+  <label>Sort:</label>
+  
+  <button class="sort" data-sort="myorder:asc">Asc</button>
+  <button class="sort" data-sort="myorder:desc">Desc</button>
+</div>
+<div id="Container" class="container">
+  <div class="mix category-1" data-myorder="1"></div>
+  <div class="mix category-1" data-myorder="2"></div>
+  <div class="mix category-1" data-myorder="3"></div>
+  <div class="mix category-2" data-myorder="4"></div>
+  <div class="mix category-1" data-myorder="5"></div>
+  <div class="mix category-1" data-myorder="6"></div>
+  <div class="mix category-2" data-myorder="7"></div>
+  <div class="mix category-2" data-myorder="8"></div>
+  
+  <div class="gap"></div>
+  <div class="gap"></div>
+</div>
+    
+    
+	<!-- /.row -->
+</div>
+<div class="testimonials top-buffer">
+	<div class="container">
+		<div class="carousel slide" data-ride="carousel" id=eventObject "carousel-example-generic">
+			<!-- Indicators -->
+			<!--<ol class="carousel-indicators">
+                    <li class="active" data-slide-to="0" data-target=
+                    "#carousel-example-generic">
+                    </li>
+                    <li data-slide-to="1" data-target=
+                    "#carousel-example-generic">
+                    </li>
+                    <li data-slide-to="2" data-target=
+                    "#carousel-example-generic">
+                    </li>
+                </ol> -->
+			<!-- Wrapper for slides -->
+			<h2>Testimonials
+			<hr />
+			</h2>
+			<div class="carousel-inner">
+				<div class="item active">
+					<div class="row">
+						<div class="col-md-12">
+							<div class="center-align">
+								<p>
+									 Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+								</p>
+								<div class="test_owner">
+									<img src="images/man.jpg" class="testimonialimg img-responsive pull-left" />
+									<p class="testowner">
+									<i>	Manish R </i>
+<br />
+										<small>Owner of apartment</small>
+									</p>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class="item">
+					<div class="row">
+						<div class="col-md-12">
+							<div class="center-align">
+								<p>
+									 Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+								</p>
+								<div class="test_owner">
+									<img src="images/man.jpg" class="testimonialimg img-responsive pull-left" />
+									<p class="testowner">
+									i>	Manish R </i>
+<br />
+										<small>Owner of apartment</small>
+									</p>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+			<!-- Controls -->
+			<a class="left carousel-control" data-slide="prev" href="#carousel-example-generic"><span class="glyphicon glyphicon-chevron-left"></span></a><a class="right carousel-control" data-slide="next" href="#carousel-example-generic"><span class= "glyphicon glyphicon-chevron-right"></span></a>
+		</div>
+	</div>
+</div>
+
+<div class="top-buffer">
+<div class="container">
+	<div class="row">
+		<div class="col-md-12 descp">
+			<h2 class=" center-block descp sc_title">
+			News <strong>&amp;</strong> Events</h2>
+			<p>
+				News and Events at Maestro Real Estate
+			</p>
+		</div>
+	</div>
+    <div class="top-buffer"></div>
+    <div class="container">
+            <div class="row">
+         
+                   
+               
+            <div class="col-md-8 news"> 
+            <img class="img-responsive img-hover pull-left" src="http://placehold.it/302x222" alt=""/>
+               
+                    <a href="blog-post.html">Blog Post Title</a>
+                
+            
+                
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
+         
+            </div>
+    
+            <div class="clear"></div>
+         
+            <div class="col-md-4 news">
+           
+                    <img class="img-responsive pull-left img-hover" src="http://placehold.it/65x65" alt="">
+                
+                
+                    <a href="blog-post.html">Blog Post Title</a>
+               
+                
+               
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, eu fugiat nulla pariatur.</p>
+                
+            </div>
+            
+            
+            
+         
+            <div class="col-md-4 news">
+           
+                    <img class="img-responsive pull-left img-hover" src="http://placehold.it/65x65" alt="">
+                
+                
+                    <a href="blog-post.html">Blog Post Title</a>
+               
+                
+                
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, eu fugiat nulla pariatur.</p>
+                
+            </div>
+            
+            
+            
+                     
+            <div class="col-md-4 news">
+        
+                    <img class="img-responsive pull-left img-hover" src="http://placehold.it/65x65" alt="">
+                
+                
+                    <a href="blog-post.html">Blog Post Title</a>
+                
+                
+                
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, eu fugiat nulla pariatur.</p>
+                
+           
+            
+            </div>
+            </div>
+            </div>
+        </div>
+        
+        
+        
+        
+</div>
+<div  class="top-buffer"></div>
+<div class="container maps">
+	<div class="row">
+		<div class="col-md-12 descp">
+			<h2 class=" center-block descp sc_title">
+			<strong>Our</strong> Corporate Office</h2>
+			<p>
+				We are Located at
+			</p>
+		</div>
+	</div>
+</div>
+
+<div class="top-buffer">
+</div>
+<div class="wrapper maps">
+<div id="map">
+</div>
+
+<div class="contact_placeholder">
+<ul>
+<li><i class="fa fa-map-marker"></i> 
+ 2277 Lorem Ave, San Diego, CA 22553 </li>
+<li> <i class="fa fa-clock-o"></i> Monday - Saturday
+from 8:00 to 20:00 <br /> <br />
+
+Free day - Sunday </li><br /> <br />
+<li><i class="fa fa-phone"></i> 
+ 123-459-7890 </li>
+<li><i class="fa fa-envelope"></i> 
+info@maestro.com </li>
+
+</ul>
+</div>
+
+</div>
+<?php include 'footer.php';?>
